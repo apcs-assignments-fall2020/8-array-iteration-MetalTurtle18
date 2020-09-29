304d6fb79af89cca20f03bd6506068518c4d0a21
MetalTurtle <dekolis1218@gmail.com>
2020-09-29 10:15:49 -0700

    Not done yet

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -2,14 +2,22 @@ public class MyMain {
 
     // Reverses an array
     public static int[] reverse(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return null;
+        int[] output = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) output[output.length - i - 1] = arr[i];
+        return output;
     }
 
     // Finds the second largest number in an array
     public static int secondLargest(int[] arr) {
-        // REPLACE WITH YOUR CODE HERE
-        return -1;
+        int max = Integer.MIN_VALUE;
+        int secondMax = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length - 1; i++)
+            if (arr[i] >= arr[i + 1])
+                max = arr[i];
+        for (int i = 0; i < arr.length - 1; i++)
+            if (arr[i] >= arr[i + 1] && arr[i] != max)
+                secondMax = arr[i];
+        return (max);
     }
 
     // Checks to see if an array contains a geometric series
@@ -20,6 +28,7 @@ public class MyMain {
 
 
     public static void main(String[] args) {
-        // YOUR CODE HERE
+        int[] a = {-1, -2, -3, -4, -8, -2, -127};
+        System.out.println(secondLargest(a));
     }
 }
